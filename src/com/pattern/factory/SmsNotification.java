83d89ceb75705064b20f27454bb84b1fecb48cfa
sohan.varadkar <sohanvardakar@gmail.com>
2021-08-19 12:55:48 +0530

    Added Factory Pattern

--- src/com/pattern/factory/SmsNotification.java
+++ src/com/pattern/factory/SmsNotification.java
@@ -3,6 +3,6 @@ package com.pattern.factory;
 public class SmsNotification implements  Notification{
     @Override
     public void notifyUser() {
-        System.out.println("Sms Notification Sent1");
+        System.out.println("Sms Notification SentS");
     }
 }
