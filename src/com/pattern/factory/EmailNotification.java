c50edf8b113447fe918795d7bf411b55f4b25d8c
sohan.varadkar <sohanvardakar@gmail.com>
2021-08-28 21:01:39 +0530

    Added Adapter And Builder1

--- src/com/pattern/factory/EmailNotification.java
+++ src/com/pattern/factory/EmailNotification.java
@@ -3,6 +3,6 @@ package com.pattern.factory;
 public class EmailNotification implements Notification{
     @Override
     public void notifyUser() {
-        System.out.println("Email Notification Sent");
+        System.out.println("Email Notification Sent1");
     }
 }
